7883a3496c3a53cd179afc245532be9b37df186e
Bharat Agarwal <43988371+bharatagsrwal@users.noreply.github.com>
2019-06-25 00:32:47 +0530

    delete and id route added and populate to get route

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,13 +1,16 @@
 //jshint esversion:6
 
 const express = require('express');
-const router = express.Router();
 const mongoose = require('mongoose');
 const orderModel = require("../models/orders.model");
 const productModel = require("../models/products.model");
 
+const router = express.Router();
+
 router.get("/", (req, res, next) => {
     orderModel.find({}, {
+        __v: 0
+    }).populate('product', { //.populate to get data from ref which added in model 
         __v: 0
     }).exec().then((docs) => { //.select('quantity productID')
         console.log(docs);
@@ -17,7 +20,7 @@ router.get("/", (req, res, next) => {
                 orders: docs.map(doc => {
                     return {
                         _id: doc._id,
-                        productID: doc.product,
+                        product: doc.product,
                         quantity: doc.quantity
                     };
                 })
@@ -64,16 +67,44 @@ router.post("/", (req, res, next) => {
 });
 
 router.get("/:orderID", (req, res, next) => {
-    res.status(200).json({
-        message: "Orders Details",
-        id: req.params.orderID,
+    orderModel.findById(req.params.orderID, {
+        __v: 0
+    }).exec().then(order => {
+        console.log(order);
+        if (!order) {
+            res.status(404).json({
+                message: 'No Valid entry Found'
+            });
+        } else {
+            res.status(200).json({
+                order
+            });
+        }
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
 router.delete("/:orderID", (req, res, next) => {
-    res.status(200).json({
-        message: "Orders Deleted",
-        id: req.params.orderID,
+    const id = req.params.orderID;
+    orderModel.remove({
+        _id: id
+    }).exec().then(result => {
+        if (!result) {
+            res.status(404).json({
+                message: 'No Valid entry Found'
+            });
+        } else {
+            res.status(200).json(result);
+        }
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err,
+        });
     });
 });
 
