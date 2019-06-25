23f0f0e50446db4178e8a76ea9498f6de99103e4
Bharat Agarwal <43988371+bharatagsrwal@users.noreply.github.com>
2019-06-25 08:08:21 +0530

    File upload is setup using MULTER

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,9 +1,37 @@
 // jshint esversion:6
 const express = require("express");
-const router = express.Router();
 const mongoose = require('mongoose');
+const multer = require('multer');
 
 const ProductMo = require('../models/products.model');
+const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5,
+    },
+    fileFilter: fileFilter
+});
+
+
 
 router.get("/", (req, res, next) => {
     ProductMo.find({}, {
@@ -42,8 +70,8 @@ router.get("/", (req, res, next) => {
 
 
 
-router.post("/", (req, res, next) => {
-
+router.post("/", upload.single('productImage'), (req, res, next) => {
+    console.log(req.file);
     const product = new ProductMo({
         _id: mongoose.Types.ObjectId(),
         name: req.body.name,
